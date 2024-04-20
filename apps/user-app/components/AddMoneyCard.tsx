e2110c6b7ac51d0145aa837cd2121ef0626d4427
Sahib.Aujla <sahibpreet568@gmail.com>
2024-04-20 14:12:50 -0400

    complete code

---
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+import { Select } from "@repo/ui/select";
+import { useState } from "react";
+import { TextInput } from "@repo/ui/textinput";
+import { createOnRampTransaction } from "../lib/createOnRampTransaction";
+const SUPPORTED_BANKS = [
+  {
+    name: "HDFC Bank",
+    redirectUrl: "https://netbanking.hdfcbank.com",
+  },
+  {
+    name: "Axis Bank",
+    redirectUrl: "https://www.axisbank.com/",
+  },
+];
+
+export const AddMoney = () => {
+  const [redirectUrl, setRedirectUrl] = useState(
+    SUPPORTED_BANKS[0]?.redirectUrl
+  );
+  const [amount, setAmount] = useState(0);
+  const [provider,setProvider]=useState(SUPPORTED_BANKS[0]?.name || "");
+
+  const onAddMoney = async() => {
+
+    await createOnRampTransaction(provider,amount);
+    window.location.href = redirectUrl || "";
+  };
+  return (
+    <Card title="Add Money">
+      <div className="w-full">
+        <TextInput
+          label={"Amount"}
+          placeholder={"Amount"}
+          onChange={(val)=>{setAmount(Number(val))}}
+        />
+        <div className="py-4 text-left">Bank</div>
+        <Select
+          onSelect={(value) => {
+            setProvider(SUPPORTED_BANKS.find((x) => x.name === value)?.name || "")
+            setRedirectUrl(
+              SUPPORTED_BANKS.find((x) => x.name === value)?.redirectUrl || ""
+            );
+          }}
+          options={SUPPORTED_BANKS.map((x) => ({
+            key: x.name,
+            value: x.name,
+          }))}
+        />
+        <div className="flex justify-center pt-4">
+          <Button onClick={onAddMoney}>Add Money</Button>
+        </div>
+      </div>
+    </Card>
+  );
+};
